a7ac799d4b490a79c1c84520b4d02bcb2d884641
Ross Parsons <rparsons779@gmail.com>
2020-11-27 20:15:53 -0500

    updated Customer class

--- src/com/finalproject/group11/model/Customer.java
+++ src/com/finalproject/group11/model/Customer.java
@@ -5,6 +5,12 @@ public class Customer extends User{
     private Cart cart;
     private double balance;
 
+
+    /**
+     * Customer constructor calls the super constructor and adds to additional attributes,
+     * a Cart object and a balance.
+     * note: balance is set at time of custmer creation
+     * */
     public Customer(int id, String name, String password, Cart cart, double balance) {
         super(id, name, password);
         this.cart = cart;
@@ -14,16 +20,24 @@ public class Customer extends User{
 
     /**
      * add_product_to_cart() - adds a product to the shopping cart
-     *
+     * @precondition: product exists in Inventory
+     * @postcondition: product is added to cart object
      * */
     public void add_product_to_cart(Product product){}
 
-    /***/
+    /**
+     * view_product() - brings up a swing component displaying product info
+     * note: 'void' may not be the return value, and instead a swing component
+     * */
     public void view_product(Product product){}
 
-    /***/
-    public void browse_product(Product product){}
 
-    /***/
+    /**
+     * checkout() - brings customer to checkout pane
+     * note: 'void' may not be the return value, and instead a swing component
+     *
+     * @precondition:
+     * @postcondition:
+     * */
     public void checkout(){}
 }
