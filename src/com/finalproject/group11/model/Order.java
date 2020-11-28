59a832a187eb3fbff20172fde6fd3e95d0cca24f
Matthew Taylor <mtaylor2096@gmail.com>
2020-11-28 18:51:32 -0500

    Added basic functionality for Order model

--- src/com/finalproject/group11/model/Order.java
+++ src/com/finalproject/group11/model/Order.java
@@ -1,5 +1,47 @@
 package com.finalproject.group11.model;
 
-public class Order {
+import java.util.ArrayList;
+import java.util.Date;
 
+public class Order {
+	int id;
+	double total = 0;
+	Cart cart;
+	Date timestamp = new Date();
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public Cart getCart() {
+		return cart;
+	}
+	public void setCart(Cart cart) {
+		this.cart = cart;
+	}
+	
+	private ArrayList<Product> getProducts() {
+		return cart.getProductList();
+	}
+	
+	public double getTotal() {
+		this.getProducts().forEach(product -> {
+			total += product.getPrice();
+		});
+		return total;
+	}
+	
+	public void checkout() {
+		
+	}
+	
+	private void remove_from_inventory(ArrayList<Product> products) {
+//		products.stream().map(product-> {
+//			product.setQuantity(product.getPrice() - 1);
+//		})
+		
+	}
 }
