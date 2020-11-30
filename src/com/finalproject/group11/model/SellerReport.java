ba4337a94f3af33bcf9021b89ddb78f101124d47
jdjoum <63579019+jdjoum@users.noreply.github.com>
2020-11-30 17:02:07 -0500

    Seller and SellerReport Update
    Seller and SellerReport Update

--- src/com/finalproject/group11/model/SellerReport.java
+++ src/com/finalproject/group11/model/SellerReport.java
@@ -2,4 +2,26 @@ package com.finalproject.group11.model;
 
 public class SellerReport {
 
+    // SellerReport Member Variables
+    private int seller_id;
+    private double profit;
+    private double revenue;
+    private int sales;
+
+    // SellerReport Constructor
+    public SellerReport(int seller_id) {
+        this.seller_id = seller_id;
+        this.profit = 0;
+        this.revenue = 0;
+        this.sales = 0;
+    }
+
+    // SellerReport Methods
+    public void update_seller_report() {
+        // Update the profit, revenue, and sales every time items are sold
+    }
+
+    public void display_report() {
+        // Save the current SellerReport values, make the SellerReport, and show it to the screen
+    }
 }
