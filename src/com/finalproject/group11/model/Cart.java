5f19147c9decb097292c4288fd561671ff2c98c5
jdjoum <63579019+jdjoum@users.noreply.github.com>
2020-11-30 16:21:58 -0500

    Cart Class Update
    Cart Class Update

--- src/com/finalproject/group11/model/Cart.java
+++ src/com/finalproject/group11/model/Cart.java
@@ -1,5 +1,25 @@
 package com.finalproject.group11.model;
 
+import java.util.ArrayList;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Cart {
 
+    // Member Variables
+    private int id;
+    private int seller_id;
+    private ArrayList<Product> products_list;
+    private DateTimeFormatter formatter;
+    private LocalDateTime timestamp;
+
+    // Cart Constructor
+    public Cart() {
+        products_list = new ArrayList<Product>();
+    }
+
+    // Cart Method
+    public void add_product(Product product){
+        this.products_list.add(product);
+    }
 }
